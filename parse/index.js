e50022f3e94df4463a761d90152be44e1f23f7b1
qrobinso <qrobinso@gmail.com>
2017-01-11 23:44:46 -0500

    revert1

--- parse/index.js
+++ parse/index.js
@@ -12,10 +12,10 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://parsemigration:parsemigration@ds159988.mlab.com:59988/moments',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'ZvCfdguFPSNNDYaTVvfHZnvFu5IT2UheXc5lSChA',
+  masterKey: process.env.MASTER_KEY || 'b9OVGpWd7PS7UjRYYtycJXJ6Z65DrGxGKtUDCXKC', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
